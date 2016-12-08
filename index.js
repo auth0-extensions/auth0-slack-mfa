e53437d2c7d01076ec1250967d1e256f7f6b9ab2
Shawn Meyer <sgmeyer@gmail.com>
2016-12-08 16:26:23 -0600

    Adding default temporary route for testing.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var express    = require('express');
-// var bodyParser = require('body-parser');
-// var auth0      = require('auth0-oauth2-express');
-// var Webtask    = require('webtask-tools');
+var bodyParser = require('body-parser');
+var auth0      = require('auth0-oauth2-express');
+var Webtask    = require('webtask-tools');
 var app        = express();
 
-// app.use(auth0({
-//   scopes: 'update:users'
-// }));
+app.use(auth0({
+  scopes: 'update:users'
+}));
 
 // var controllers = [
 //   require('./controllers/cancel'),
@@ -16,18 +16,22 @@ var app        = express();
 //   require('./controllers/meta.js')
 // ];
 
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use('/', controllers);
+app.use(bodyParser.urlencoded({ extended: false }));
+//app.use('/', controllers);
 
-// app.use(function (req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
+app.get('/', function (req, res) {
+  res.send('Dummy bits');
+});
 
-// app.use(function (err, req, res, next) {
-//   console.log('Application Error Handler: ' + err + '\r\nStack: \r\n' + err.stack);
-//   res.status(err.status || 500).send("Oh no!  This is pretty embarrassing").end();
-// });
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function (err, req, res, next) {
+  console.log('Application Error Handler: ' + err + '\r\nStack: \r\n' + err.stack);
+  res.status(err.status || 500).send("Oh no!  This is pretty embarrassing").end();
+});
 
 module.exports = app;
