0e6ba8b9d7071d6426149eade2f58193138e0379
Shawn Meyer <sgmeyer@gmail.com>
2016-12-20 12:38:32 -0600

    Fixing config issue

--- build/webpack/config.base.js
+++ build/webpack/config.base.js
@@ -8,7 +8,6 @@ module.exports = {
 
   // The application and the vendor libraries.
   entry: {
-    app: path.resolve(__dirname, '../../client/app.jsx'),
     vendors: [
       'axios',
       'bluebird',
--- server/lib/compileRule.js
+++ server/lib/compileRule.js
@@ -8,6 +8,6 @@ export default () =>
     apiKey: config('EXTENSION_SECRET'),
     updateTime: () => new Date().toISOString(),
     SLACK_API_TOKEN: config('SLACK_API_TOKEN'),
-    SIGINING_SECRET: cofnig('SIGNING_SECRET'),
+    SIGINING_SECRET: config('SIGNING_SECRET'),
     MONGO_CONNECTION_STRINGT: config('MONGO_CONNECTION_STRING')
   });
